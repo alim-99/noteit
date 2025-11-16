b475c41f23690009294b6ee156415de03ac66eba
alim-99 <alim16097side@gmail.com>
2025-11-17 00:00:31 +0200

    sign in theme changed

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,18 @@ import { SignIn } from "@clerk/nextjs"
 const page = () => {
       return (
             <main className="flex items-center justify-center mt-10">
-                  <SignIn />
+                  <SignIn
+                        appearance={{
+                              variables: {
+                                    colorPrimary: "black",
+                                    colorBackground: "#C5AAEE",
+                                    colorInputBackground: "#080410",
+                                    colorInputText: "#F9FAFB",
+                                    colorText: "black",
+                                    colorTextSecondary: "#6c6970",
+                              },
+                        }}
+                  />
             </main>
       )
 }
