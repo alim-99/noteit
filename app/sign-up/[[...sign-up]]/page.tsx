cba937e7ad596e840549360532f805bac3763d00
alim-99 <alim16097side@gmail.com>
2025-12-03 00:03:28 +0200

    signup styles fixed

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,18 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
       return (
             <main className="flex items-center justify-center mt-10">
-                  <SignUp />
+                  <SignUp
+                        appearance={{
+                              variables: {
+                                    colorPrimary: "black",
+                                    colorBackground: "#C5AAEE",
+                                    colorInputBackground: "#080410",
+                                    colorInputText: "#F9FAFB",
+                                    colorText: "black",
+                                    colorTextSecondary: "#6c6970",
+                              },
+                        }}
+                  />
             </main>
       );
 }
